75d43dbe0bc242ea0e14cf517adca85f3b4c1219
opilane <opilane@HP-CZC6297B8H>
2021-10-29 11:30:59 +0300

    andrei

--- Andrei 29.10/Andrei_29.10.py
+++ Andrei 29.10/Andrei_29.10.py
@@ -1,19 +1,20 @@
-a = [0]*4 
-import random
+a = [0]*4
+from random import *
+from keyboard import *
 for i in range (4):
     ver = 0
     while (ver==0):
         player = int(input("1 - kivi, 2 - käärid, 3 - paaber. "))
         if (player == 1 or player == 2 or player == 3):
             ver = 1
-    a[i] = player     
+    a[i] = player    
     if player == 1:
         print("Olete valinud kivi.")
     if player == 2:
         print("Sa valisid käärid.")
     if player == 3:
         print("Valisite paberi.")
-    comp = random.randint(1,3)
+    comp=randint(1,3)
     if comp == 1:
         print("Arvuti valis kivi.")
     if comp == 2:
@@ -23,7 +24,7 @@ for i in range (4):
     if player == comp:
         win = 0
     if player == 1 and comp == 2:
-        win = 1 
+        win = 1
     if player == 1 and comp == 3:
         win = 2
     if player == 2 and comp == 1:
@@ -59,7 +60,7 @@ if n>2:
     comp = 1
 if b>2:
     comp = 2
-    
+   
 while (ver==0):
     player = int(input("1 - kivi, 2 - käärid, 3 - paaber. "))
     if (player == 1 or player == 2 or player == 3):
@@ -79,7 +80,7 @@ if comp == 3:
 if player == comp:
         win = 0
 if player == 1 and comp == 2:
-        win = 1 
+        win = 1
 if player == 1 and comp == 3:
         win = 2
 if player == 2 and comp == 1:
@@ -96,14 +97,68 @@ if win == 1:
         print("Võitis mängija!")
 if win == 2:
         print("Võitnud arvuti!")
-
-
-
-v1=["kivi, käärid, paber"]
-v2=["kivi, käärid, paber"]
-if m==1:
+v1=["kivi","käärid","paber"]
+v2=["kivi","käärid","paber"]
+b=0
+c=0
+p=''
+from random import*
+while p not in [1,2]:
+    try:
+        p=int(input("Выберите с кем хотите играть:\n1=игра с компом\n2=игра ботов"))
+    except:
+        TypeError
+if p==1:
     while True:
-        print("Kas mängime? esc välja, enter-mangime")
+        a=''
+        while a not in [1,2,3,4]:
+            try:
+                a=int(input("Выберите камень=1, бумага=2, ножницы=3, закончить игру=4  ="))
+            except:
+                TypeError
+        if a==4: break
+        z=randint(1,3)
+        if z==1:
+            print("Компьютер выбрал камень")
+        elif z==2:
+            print("Компьютер выбрал бумага")
+        else:
+            print("Компьютер выбрал ножницы")
+        if a==1 and z==3:
+            print("Ты выйграл")
+            b+=1
+        elif a==z:
+            print("Ничья")
+        elif a==2 and z==3:
+            print("Ты проиграл")
+            c+=1
+        elif a==3 and z==2:
+            print("Ты выйграл")
+            b+=1
+        elif a==3 and z==1:
+            print("Ты проиграл")
+            c+=1
+        elif a==2 and z==1:
+            print("Ты выйграл")
+            b+=1
+        elif a==1 and z==2:
+            print("Ты проиграл")
+            c+=1
+elif p==2:
+    v1=["Камень","Ножницы","Бумага"]
+    v2=["Камень","Ножницы","Бумага"]
+    while True:
+        print("Играем? esc= выходим, enter= играем")
         if read_key()=="esc":
             break
         elif read_key()=="enter":
+            p1=choice(v1)
+            print("Первый Бот: ",p1)
+            p2=choice(v2)
+            print("Второй Бот: ",p2)
+            if p1==p2:
+                print("Ничья")
+            elif p1==v1[0] and p2==v2[1] or p1==v1[2] and p2==v2[0] or p1==v1[1] and p2==v2[2]:
+                print("Выйграл 1")
+            else:
+                print("Выйграл 2")
