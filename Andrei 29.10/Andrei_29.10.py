a5f437ee77a942004f9686990ffdc213a218c1f7
opilane <opilane@HP-CZC6297B8H>
2021-10-29 09:02:10 +0300

    фффф

--- Andrei 29.10/Andrei_29.10.py
+++ Andrei 29.10/Andrei_29.10.py
@@ -1,1 +1,13 @@
-print("hello world!")
+from random import*
+m=3
+print("Камень Ножницы Бумага")
+a=0 
+print()
+while ans not in [1,2,3]:
+    try:
+        ans=int(input("Выбери камень ножницы или бумагу"))
+        except:
+            ValueError
+
+    
+
